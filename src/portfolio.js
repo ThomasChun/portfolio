4793a032f0bb1aa1ceedbf82ec175cd1c42a7aa0
Thomas Chun <tchun2055@gmail.com>
2019-02-08 11:13:25 -0800

    added imgs and flexbox css

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,6 +6,24 @@ import Header from './header';
 
 class Portfolio extends Component {
   render() {
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+      } else {
+        document.getElementById("topBtn").style.display = "none";
+      }
+    }
+
+    function topFunction() {
+      console.log('clicked');
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+
     return (
       <div className="portfolio">
         <Header />
@@ -14,6 +32,7 @@ class Portfolio extends Component {
         <Projects />
         <hr />
         <Footer />
+        <button onClick={() => topFunction()} id="topBtn" title="back to top"><img src="icons8-scroll-up-48.png" className="up-arrow" alt="back to top"/></button>
       </div>
     );
   }
