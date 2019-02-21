a93b51f2c050ad14e85925e36773868266d7e9ff
Thomas Chun <tchun2055@gmail.com>
2019-02-21 15:14:52 -0800

    clicking on resume now links to resume.pdf

--- src/about.js
+++ src/about.js
@@ -8,7 +8,7 @@ class About extends Component {
         <div className="about-me-content">
           <div className="doc-links">
             <img src="icons8-resume-30.png" alt="Resume" />
-            <div className="resume"><a className="resume" href="https://docs.google.com/document/d/1T8qI_zEVNNkpnHqQhUyP3T0K2pxAhN9FyBzG609OSSk/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></div>
+            <div className="resume"><Link className="resume" to="/resume" target="_blank" rel="noopener noreferrer">Resume</Link></div>
             <img src="icons8-linkedin-48.png" alt="LinkedIn" />
             <div className="linkedin"><a className="linkedin" href="https://www.linkedin.com/in/thomas-chun-99385a95/" target="_blank" rel="noopener noreferrer">LinkedIn</a></div>
             <img src="icons8-github-48.png" alt="GitHub" />
--- src/footer.js
+++ src/footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
       <div className="footer">
         <div className="footer-links">
           <img src="icons8-resume-24.png" alt="Resume"/>
-          <div className="footer-resume"><a className="resume" href="https://docs.google.com/document/d/1T8qI_zEVNNkpnHqQhUyP3T0K2pxAhN9FyBzG609OSSk/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></div>
+          <div className="footer-resume"><Link className="footer-resume" to="/resume" target="_blank" rel="noopener noreferrer">Resume</Link></div>
           <img src="icons8-linkedin-24.png" alt="LinkedIn"/>
           <div className="footer-linkedin"><a className="footer-linkedin" href="https://www.linkedin.com/in/thomas-chun-99385a95/" target="_blank" rel="noopener noreferrer">LinkedIn</a></div>
           <img src="icons8-github-24.png" alt="GitHub"/>
